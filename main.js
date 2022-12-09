4d4f3951e081fba12ca0c80b29a49b14a7d70d09
Santiago Mansilla <santimasig@outlook.com.ar>
2022-12-09 20:31:00 -0300

    Entre a actualizar

--- main.js
+++ main.js
@@ -1,93 +1,21 @@
-//Clase de productos
-class Producto {
-  constructor(id, nombre, precio, img, direccion) {
-    (this.id = id), (this.nombre = nombre), (this.precio = precio);
-    this.img = img;
-    this.direccion = direccion;
-  }
-}
+const divProd = document.querySelector(`#divProd`);
 
-//Luxon
-const {DateTime} = luxon;
-const ahora = DateTime.now().toLocaleString();
+const buscarProd = async () => {
+  const prodFetch = await fetch(`productos.json`);
+  const prodJson = await prodFetch.json();
 
-//Array de productos y carrito
-const productosArray = [];
-
-//Agrego los productos al Array
-productosArray.push(
-  new Producto(1, "IPHONE 13 PRO", 500, "iphone.jpg", "iphone13pro.html")
-);
-productosArray.push(new Producto(2, "REDMI NOTE 10 PRO", 400, "redminote10pro.jpg", "redminote10pro.html"));
-productosArray.push(new Producto(3, "NIKOND3500", 350, "nikond3500.jpg", "nikond3500.html"));
-productosArray.push(new Producto(4, "REDRAGON K630", 80, "reddragonk.jpg", "reddragonk630.html"));
-
-//Elementos del DOM
-const divProductos = document.getElementById("divProductos");
-
-divProductos.innerHTML = `
-    <h1 class="h3 text-center mt-3 mb-4">Bienvenido/a </h1>
-    <div class="row justify-content-around">
-    `;
-
-productosArray.forEach((producto) => {
-  divProductos.innerHTML = divProductos.innerHTML + `
-    <div id="${producto.id}" class="card" style="width: 18rem">
-        <img src="./images/productos/${producto.img}" class="card-img-top" alt="Imagen de Iphone 13 Pro" />
+  prodJson.forEach(prod => {
+    divProd.innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src="${prod.imagen}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h4 class="card-title text-center">${producto.nombre}</h4>
-          <p class="card-text">${producto.precio}</p>
-          <a href="./pages/productos/${producto.direccion}" class="btn btn-primary">Ir al Producto</a>
-          <button id="${producto.id}" class="sumarCar">Comprar</button>
+            <h5>${prod.nombre}</h5>
+            <p class="card-text">${prod.precio}</p>
+            <button id=${id} class"btn btn-primary">Agregar</button>
         </div>
-      </div>
+    </div>
     `;
-});
-
-//Carrito
-const carrito = [];
-const botonAgregar = document.querySelectorAll(".sumarCar"); //Por alguna razon con GetElement no funciona
-
-//Boton comprar 
-botonAgregar.forEach((boton) => {
-  boton.onclick = () => {
-    const producto = productosArray.find(
-      (prod) => prod.id === parseInt(boton.id)
-    );
-
-    Toastify({
-      text:"Se añadio (" +producto.nombre + ") con un precio de: " + producto.precio,
-      className: "info",
-      style: {background: "linear-gradient(to right, #00b09b, #96c93d)"},
-    }).showToast();
-
-    const productoCarrito = {
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      cantidad: 1,
-    };
-
-    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id);
-
-    if (indexCarrito === -1) {
-      carrito.push(productoCarrito);
-    } else {
-      carrito[indexCarrito].cantidad += 1;
-    }
-  };
-});
-
-//Boton finalizar compra
-
-const bttnFin = document.getElementById("finalizar");
-
-bttnFin.onclick = () => {
-  const totalCompra = carrito
-    .map((prod) => prod.precio * prod.cantidad)
-    .reduce((elem1, elem2) => elem1 + elem2);
-  Swal.fire("El total de tu compra es: " + totalCompra + "\n" + ahora);
-
-};
-
-
+  });
+  
+  
+}
